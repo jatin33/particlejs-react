62c0bf1dbb80d9428a2adf100c36c7662126cda5
jatin33 <jatinpanjwani12@gmail.com>
2019-12-29 03:32:32 +0530

    Particle js integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import logo from './logo.svg';
+import nodelogo from './nodejs.svg';
+
+const particleOptions = {
+	    "particles": {
+	        "number": {
+	            "value": 8,
+	            "density": {
+	                "enable": true,
+	                "value_area": 800
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "speed": 1,
+	            "out_mode": "out"
+	        },
+	        "shape": {
+	            "type": [
+	                "images",
+	                "circle"
+	            ],
+	            "images": [
+	                {
+	                    "src":logo,
+	                    "height": 20,
+	                    "width": 23
+                  },
+                  {
+                    "src":nodelogo,
+                    "height":20,
+                    "width":23
+                  }
+	            ]
+	        },
+	        "color": {
+	            "value": "#CCC"
+	        },
+	        "size": {
+	            "value": 30,
+	            "random": false,
+	            "anim": {
+	                "enable": true,
+	                "speed": 4,
+	                "size_min": 10,
+	                "sync": false
+	            }
+	        }
+	    },
+	    "retina_detect": false
+	};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{color:'white'}}>Hello World</h1>
+      <Particles  params={particleOptions}/> 
     </div>
   );
 }
